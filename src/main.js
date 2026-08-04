ebf7aa94926d8116855a7754668ecee70c055d9c
joacoContreras <cjoaquin835@gmail.com>
2026-08-04 13:44:37 +0000

    a beatiful IU, implemented definitive css

--- src/main.js
+++ src/main.js
@@ -2,30 +2,35 @@
 import { calculate , negate} from './operations.js';
 
 const calculatorButtons = [
-    {text: 'AC', type: 'action', value: 'clear'},
-    {text: '+/-', type: 'action', value: 'negate'},
     {text: '%', type: 'action', value: 'percent'},
-    {text: '+', type: 'operator', value: '+'},
+    {text: 'AC', type: 'action', value: 'clear'},
+    {text: 'C', type: 'action', value: ''},
+    {text: '⌫', type: 'action', value: 'backspace'},
+
+    {text: '1/x', type: 'action', value: 'inverse'},
+    {text: 'x^2', type: 'action', value: 'power'},
+    {text: 'sqrt(x)', type: 'action', value: 'sqrt'},
     {text: '÷', type: 'operator', value: '/'},
-    {text: 'x', type: 'operator', value: '*'},
-    {text: '-', type: 'operator', value: '-'},
-    {text: '=', type: 'equals', value: '='},
 
-    { text: '9', type: 'number', value: '9' },
-    { text: '8', type: 'number', value: '8' },
     { text: '7', type: 'number', value: '7' },
-    
-    { text: '6', type: 'number', value: '6' },
-    { text: '5', type: 'number', value: '5' },
-    { text: '4', type: 'number', value: '4' },
-    
-    { text: '3', type: 'number', value: '3' },
-    { text: '2', type: 'number', value: '2' },
-    { text: '1', type: 'number', value: '1' },
-    
-    { text: '0', type: 'number', value: '0' },
-    { text: '.', type: 'number', value: '.' }
+    { text: '8', type: 'number', value: '8' },
+    { text: '9', type: 'number', value: '9' },
+    {text: 'x', type: 'operator', value: '*'},
 
+    { text: '4', type: 'number', value: '4' },
+    { text: '5', type: 'number', value: '5' },
+    { text: '6', type: 'number', value: '6' },
+    {text: '-', type: 'operator', value: '-'},
+
+    { text: '1', type: 'number', value: '1' },
+    { text: '2', type: 'number', value: '2' },
+    { text: '3', type: 'number', value: '3' },
+    {text: '+', type: 'operator', value: '+'},
+
+    {text: '+/-', type: 'equals', value: 'negate'},
+    { text: '0', type: 'number', value: '0' },
+    { text: '.', type: 'number', value: '.' },
+    {text: '=', type: 'equals', value: '='}
 ];
 
 // 1. Seleccionamos el contenedor padre en el DOM
@@ -41,7 +46,7 @@ function createCalculatorKeys() {
         button.textContent = btnInfo.text;
         
         // c. Asignar clases para CSS
-        button.classList.add('key', 'key--${btnInfo.type}');
+        button.classList.add('key', `key--${btnInfo.type}`);
 
         // d. Guardar metadatos usando data attributes
         button.dataset.type = btnInfo.type; // Se genera: data-type= "..."
